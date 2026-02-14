aaf7e13dc530244b12278ae96dd7e2eb206fab74
MustaqAli99 <mustaqali162@gmail.com>
2026-02-14 10:43:59 +0530

    changed DBconnection file for deploymant process

--- src/main/java/com/expense/dao/DBConnection.java
+++ src/main/java/com/expense/dao/DBConnection.java
@@ -3,18 +3,31 @@ package com.expense.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+//public class DBConnection {
+//    public static Connection getConnection() {
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            return DriverManager.getConnection(
+//                "jdbc:mysql://localhost:3306/expense_tracker",
+//                "root",
+//                "root"
+//            );
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//}
 public class DBConnection {
-    public static Connection getConnection() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/expense_tracker",
-                "root",
-                "root"
-            );
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+
+    public static Connection getConnection() throws Exception {
+
+        String url = System.getenv("DB_URL");
+        String user = System.getenv("DB_USER");
+        String pass = System.getenv("DB_PASS");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(url, user, pass);
     }
 }
+
